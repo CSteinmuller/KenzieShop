ae26d6873f6139f90c50e9095eb2aaa44a586ad7
CSteinmuller <chris.dungeonmaster@gmail.com>
2018-03-10 15:31:48 -0600

    Comments
    Added Todo

--- KenzieAnimeShop/Program.cs
+++ KenzieAnimeShop/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
+/**
+ TO DO:
+    1) Fix final formatting
+
+    */
 
 namespace KenzieAnimeShop
 {
